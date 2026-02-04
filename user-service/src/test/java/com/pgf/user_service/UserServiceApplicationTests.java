79b06d65aadd5bbe9da284460c6c9433eadc675b
shubham pandey <shubhampandey.kumar77@gmail.com>
2026-02-04 22:44:53 +0530

    PCF: fixed the build failure

--- user-service/src/test/java/com/pgf/user_service/UserServiceApplicationTests.java
+++ user-service/src/test/java/com/pgf/user_service/UserServiceApplicationTests.java
@@ -2,14 +2,15 @@ package com.pgf.user_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false) // ✅ disables Spring Security filters safely
 class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
